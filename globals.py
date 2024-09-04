b036f44b5c4d2746f4538f16aa955f5b11c725f1
Steffen Probst <steffen.probst@fau.de>
2024-09-04 12:24:09 +0200

    feat: added sandbox

--- globals.py
+++ globals.py
@@ -5,11 +5,12 @@ from pathlib import Path
 class Globals:
     PROJ_DIR_PATH = str(Path(__file__).resolve().parent)
     PROJ_VSCODE_DIR_PATH = str(Path(PROJ_DIR_PATH) / '.vscode')
+    PROJ_ENV_FILE_PATH = str(Path(PROJ_DIR_PATH) / '.env')
+    PROJ_SANDBOX_PATH = str(Path(PROJ_DIR_PATH) / 'sandbox')
     CURRENT_WORKING_DIR_PATH = os.getcwd()  # New property for current working directory
 
     os.makedirs(PROJ_VSCODE_DIR_PATH, exist_ok=True)
     PROJ_CONFIG_FILE_PATH = str(Path(PROJ_VSCODE_DIR_PATH) / 'cli-agent.json')
-    PROJ_ENV_FILE_PATH = str(Path(PROJ_DIR_PATH) / '.env')
 
     @classmethod
     def get_path(cls, path_name: str) -> str:
